80f99aeba847249a445e9ef2c124417364a942d2
Caio Ferrarezi <caioferrarezi2@gmail.com>
2022-01-14 16:40:41 -0300

    test: change first package

--- packages/first/tests/unit/example.spec.js
+++ packages/first/tests/unit/example.spec.js
@@ -3,10 +3,12 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
+    const msg = 'changed message'
+
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
+
     expect(wrapper.text()).toMatch(msg)
   })
 })
